f8b38149692e75350c419774fd01be1198940d56
Stef Fan <stef.fan@expeditors.com>
2020-07-08 16:19:25 +0800

    add login/register

--- mars/__init__.py
+++ mars/__init__.py
@@ -47,7 +47,7 @@ def register_extensions(app):
 def register_blueprints(app):
     app.register_blueprint(main_bp)
     # app.register_blueprint(admin_bp, url_prefix='/admin')
-    # app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
 
 # def register_shell_context(app):
