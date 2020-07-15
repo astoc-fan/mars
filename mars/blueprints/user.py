444679f423c82726ac8dfd3f909fa688eea26fd1
Stef Fan <stef.fan@expeditors.com>
2020-07-15 13:01:56 +0800

    add user settings

--- mars/blueprints/user.py
+++ mars/blueprints/user.py
@@ -1,13 +1,13 @@
-from flask import render_template, flash, redirect, url_for, current_app, request, Blueprint
+from flask import render_template, flash, redirect, url_for, Blueprint
 from flask_login import login_required, current_user, fresh_login_required, logout_user
 
 # from mars.decorators import confirm_required, permission_required
 from mars.emails import send_change_email_email
 from mars.extensions import db
-from mars.forms.user import EditProfileForm
+from mars.forms.user import EditProfileForm, ChangePasswordForm, ChangeEmailForm, DeleteAccountForm
 from mars.models import User
 from mars.settings import Operations
-from mars.utils import generate_token, validate_token, redirect_back, flash_errors
+from mars.utils import generate_token, validate_token
 
 user_bp = Blueprint('user', __name__)
 
@@ -15,9 +15,11 @@ user_bp = Blueprint('user', __name__)
 @user_bp.route('/<username>')
 def index(username):
     user = User.query.filter_by(username=username).first_or_404()
+    if user == current_user and user.locked:
+        flash('Your account is locked.', 'danger')
 
-    # if user == current_user:
-    #     logout_user()
+    if user == current_user and not user.active:
+        logout_user()
 
     return render_template('user/index.html', user=user)
 
@@ -39,3 +41,53 @@ def edit_profile():
     form.branch.data = current_user.branch
     form.department.data = current_user.department
     return render_template('user/settings/edit_profile.html', form=form)
+
+
+@user_bp.route('/settings/change-password', methods=['GET', 'POST'])
+@fresh_login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.validate_password(form.old_password.data):
+            current_user.set_password(form.password.data)
+            db.session.commit()
+            flash('Password updated.', 'success')
+            return redirect(url_for('.index', username=current_user.username))
+        else:
+            flash('Old password is incorrect.', 'warning')
+    return render_template('user/settings/change_password.html', form=form)
+
+
+@user_bp.route('/settings/change-email', methods=['GET', 'POST'])
+@fresh_login_required
+def change_email_request():
+    form = ChangeEmailForm()
+    if form.validate_on_submit():
+        token = generate_token(user=current_user, operation=Operations.CHANGE_EMAIL, new_email=form.email.data.lower())
+        send_change_email_email(to=form.email.data, user=current_user, token=token)
+        flash('Confirm email sent, check your inbox.', 'info')
+        return redirect(url_for('.index', username=current_user.username))
+    return render_template('user/settings/change_email.html', form=form)
+
+
+@user_bp.route('/change-email/<token>')
+@login_required
+def change_email(token):
+    if validate_token(user=current_user, token=token, operation=Operations.CHANGE_EMAIL):
+        flash('Email updated.', 'success')
+        return redirect(url_for('.index', username=current_user.username))
+    else:
+        flash('Invalid or expired token.', 'warning')
+        return redirect(url_for('.change_email_request'))
+
+
+@user_bp.route('/settings/account/delete', methods=['GET', 'POST'])
+@fresh_login_required
+def delete_account():
+    form = DeleteAccountForm()
+    if form.validate_on_submit():
+        db.session.delete(current_user._get_current_object())
+        db.session.commit()
+        flash('Your are free, goodbye!', 'success')
+        return redirect(url_for('main.index'))
+    return render_template('user/settings/delete_account.html', form=form)
--- mars/forms/user.py
+++ mars/forms/user.py
@@ -1,9 +1,7 @@
 from flask_login import current_user
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, SelectField, PasswordField, SubmitField, BooleanField, TextAreaField, HiddenField, \
-    ValidationError
-from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, Regexp
+from wtforms import StringField, SelectField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
 
 from mars.models import User
 
@@ -12,7 +10,7 @@ class EditProfileForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
     username = StringField('Username', validators=[DataRequired(), Length(5, 20),
                                                    Regexp('^[a-zA-Z][a-zA-Z0-9-]{4,15}$',
-                                                          message='The username should contain only a-z, A-Z, 0-9 and -.')])
+                                                          message='This should contain only a-z, A-Z, 0-9 and -.')])
     branch = StringField('Branch', validators=[DataRequired(), Length(0, 10)])
     # department = StringField('Department', validators=[DataRequired(), Length(0, 20)])
     department = SelectField('Department', validators=[DataRequired()],
@@ -22,3 +20,29 @@ class EditProfileForm(FlaskForm):
     def validate_username(self, field):
         if field.data != current_user.username and User.query.filter_by(username=field.data).first():
             raise ValidationError('The username is already in use.')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Length(1, 254), Email()])
+    submit = SubmitField()
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('The email is already in use.')
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[DataRequired()])
+    password = PasswordField('New Password', validators=[
+        DataRequired(), Length(8, 128), EqualTo('password2')])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField()
+
+
+class DeleteAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
+    submit = SubmitField()
+
+    def validate_username(self, field):
+        if field.data != current_user.username:
+            raise ValidationError('Wrong username.')
