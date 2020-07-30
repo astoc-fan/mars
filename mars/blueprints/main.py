4e15770828ddb523d470f11db63450c1491de4c8
Stef Fan <stef.fan@expeditors.com>
2020-07-30 14:25:54 +0800

    add xs btn, add toolpage, add rates

--- mars/blueprints/main.py
+++ mars/blueprints/main.py
@@ -24,3 +24,8 @@ def dashboard():
 @main_bp.route('/table')
 def table():
     return render_template('main/test2.html')
+
+
+@main_bp.route('/rates')
+def rates():
+    return render_template('main/rates.html')
