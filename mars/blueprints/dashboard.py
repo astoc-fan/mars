830f347bb434bb71c18d24c66f2b7f70421d5086
Stef Fan <stef.fan@expeditors.com>
2020-09-03 09:08:56 +0800

    delete unused items

--- mars/blueprints/dashboard.py
+++ mars/blueprints/dashboard.py
@@ -1,18 +1,18 @@
-import os
+import datetime
 
 import pandas as pd
-import datetime
-from flask import render_template, Blueprint, current_app
+from flask import render_template, Blueprint
 from flask_login import login_required
 from pyecharts import options as opts
-from pyecharts.charts import Geo, Bar
+from pyecharts.charts import Geo, Bar, Line
 from pyecharts.globals import GeoType
-from mars.decorators import permission_required
-from mars.models import Dashboard
 from pyecharts.globals import ThemeType
 
+from mars.decorators import permission_required
+from mars.models import Dashboard
 
 dashboard_bp = Blueprint('dashboard', __name__)
+
 
 # data = pd.read_csv(r"D:\python\test-pyechart\OE.csv")
 # ab = []
@@ -52,12 +52,28 @@ def ede_dashboard():
     bar = (
         Bar({"theme": ThemeType.DARK})
             .add_xaxis(list(ede_by_op_count.index))
-            .add_yaxis("EDE by Create User", ede_by_op_days.to_list())
-            .add_yaxis("days outstanding", ede_by_op_count.to_list())
-            # .set_global_opts(title_opts=opts.TitleOpts(title="Line-MarkPoint"))
+            .add_yaxis("EDE by Create User", ede_by_op_days.to_list(),
+                       label_opts=opts.LabelOpts(is_show=True), )
+            # .add_yaxis("days outstanding", ede_by_op_count.to_list())
+            .set_global_opts(tooltip_opts=opts.TooltipOpts(
+                            is_show=True, trigger="axis", axis_pointer_type="cross"),
+            ),
+        )
+
+    line = (
+        Line({"theme": ThemeType.DARK})
+            .add_xaxis(list(ede_by_op_count.index))
+            .add_yaxis("days outstanding",
+                       ede_by_op_count.to_list(),
+                       label_opts=opts.LabelOpts(is_show=False),
+            )
     )
-    return render_template('dashboard/ede.html', bar=bar.dump_options_with_quotes()
+
+    bar.overlap(line)
+    return render_template('dashboard/ede.html', bar=bar.dump_options()
                            )
+
+
 
 
 @dashboard_bp.route('/tianjin')
